9d699e6acf4171af00c5d12d8e105f9a9c462d17
chl984 <55427861+chl984@users.noreply.github.com>
2024-06-19 16:24:24 +0900

    Title: 전화번호 바꾸기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240619/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i, x, y;
+    cin >> i;
+    cin.get();
+    cin >> x;
+    cin.get();
+    cin >> y;
+    cout << "010-" << y << "-" << x;
+    return 0;
+}
