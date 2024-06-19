696a77488ef57b5c07abdc39d1705dae1d83dc6b
chl984 <55427861+chl984@users.noreply.github.com>
2024-06-19 11:17:23 +0900

    Title: 데이터 교환, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

--- 240619/데이터 교환/exchange-data.cpp
+++ 240619/데이터 교환/exchange-data.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     int a = 5, b = 6, c = 7;
     int temp = c;
-    b = a, c = b, a = c;
+    c = b, b = a, a = temp;
     cout << a << endl << b << endl << c;
     return 0;
 }
