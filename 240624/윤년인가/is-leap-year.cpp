947a7f6fbb289eb9f73da9b87176485b39fedb3a
chl984 <55427861+chl984@users.noreply.github.com>
2024-06-24 18:22:53 +0900

    Title: 윤년인가, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240624/윤년인가/is-leap-year.cpp
+++ 240624/윤년인가/is-leap-year.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     int y;
     cin >> y;
-    if(y%400==0) cout << "false";
+    if((y%100==0)&&(y%400!=0)) cout << "false";
     else if(y%4==0) cout << "true";
     else cout << "false";
     return 0;
