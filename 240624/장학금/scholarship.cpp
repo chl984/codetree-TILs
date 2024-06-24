cbbd0b41fe8031a58b5ffec6c1357d3e372f50bf
chl984 <55427861+chl984@users.noreply.github.com>
2024-06-24 18:07:09 +0900

    Title: 장학금, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240624/장학금/scholarship.cpp
+++ 240624/장학금/scholarship.cpp
@@ -7,7 +7,7 @@ int main() {
     if(a<90) cout << 0;
     else {
         if(b>=95) cout << 10;
-        else if(b>90) cout << 5;
+        else if(b>=90) cout << 5;
         else cout << 0;
     }
     return 0;
